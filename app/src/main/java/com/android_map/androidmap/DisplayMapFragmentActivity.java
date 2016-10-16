72cf0b95d69125e3fef565a44a3bdf501f1eb044
Marcin Mackiewicz <m.mackiewicz@kainos.com>
2016-10-16 13:35:45 +0200

    Added extra view for displaying map fragments.

--- app/src/main/java/com/android_map/androidmap/DisplayMapFragmentActivity.java
+++ app/src/main/java/com/android_map/androidmap/DisplayMapFragmentActivity.java
@@ -1,4 +1,13 @@
 package com.android_map.androidmap;
 
-public class DisplayMapFragmentActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMapFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_map_fragment);
+    }
 }
--- app/src/main/java/com/android_map/androidmap/MainActivity.java
+++ app/src/main/java/com/android_map/androidmap/MainActivity.java
@@ -1,24 +1,26 @@
 package com.android_map.androidmap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        String NAMESPACE = "http://vladozver.org/";
-        String METHOD_NAME = "GetCategoryById";
-        String SOAP_ACTION = "http://vladozver.org/GetCategoryById";
-        String URL = "http://192.168.1.3/VipEvents/Services/CategoryServices.asmx";
-
-        SoapObject Request = new SoapObject(NAMESPACE, METHOD_NAME);
+    public void displayMapFragment(View view) {
+        startActivity(new Intent(
+                this,
+                DisplayMapFragmentActivity.class
+        ));
     }
 }
