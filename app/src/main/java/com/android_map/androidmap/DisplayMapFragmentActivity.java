6d4db2c6113e4d9051f3e3b3a54dd57ffaffff7f
Marcin Mackiewicz <m.mackiewicz@kainos.com>
2016-10-16 14:18:27 +0200

    Coordinates from 'EditText' fields are now sent from main view to map fragment view.

--- app/src/main/java/com/android_map/androidmap/DisplayMapFragmentActivity.java
+++ app/src/main/java/com/android_map/androidmap/DisplayMapFragmentActivity.java
@@ -1,13 +1,21 @@
 package com.android_map.androidmap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DisplayMapFragmentActivity extends AppCompatActivity {
+    private static final String LATITUDE = "latitude";
+    private static final String LONGITUDE = "longitude";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_map_fragment);
+
+        Intent intent = getIntent();
+        Log.i("Sent latitude ", intent.getStringExtra(LATITUDE));
+        Log.i("Sent longitude ", intent.getStringExtra(LONGITUDE));
     }
 }
--- app/src/main/java/com/android_map/androidmap/MainActivity.java
+++ app/src/main/java/com/android_map/androidmap/MainActivity.java
@@ -1,16 +1,15 @@
 package com.android_map.androidmap;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
-import org.ksoap2.SoapEnvelope;
-import org.ksoap2.serialization.SoapObject;
-import org.ksoap2.serialization.SoapSerializationEnvelope;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String LATITUDE = "latitude";
+    private static final String LONGITUDE = "longitude";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +17,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayMapFragment(View view) {
-        startActivity(new Intent(
+        Intent intent = new Intent(
                 this,
                 DisplayMapFragmentActivity.class
-        ));
+        );
+        
+        intent.putExtra(LATITUDE, ((EditText) findViewById(R.id.latitude)).getText().toString());
+        intent.putExtra(LONGITUDE, ((EditText) findViewById(R.id.longitude)).getText().toString());
+
+        startActivity(intent);
     }
 }
